2c7a08c1ef4d742cd4524aef549041a7a6e43830
Travis Walentin <travis.walentin@gmail.com>
2017-08-08 23:50:39 -0700

    given a vin, can return all the details wanted

--- autotrader_scraper.py
+++ autotrader_scraper.py
@@ -9,10 +9,8 @@ def get_car_details(vin_number):
     and then return something to query the DB with'''
 
     url = "https://www.etis.ford.com/vehicleSelection.do"
-
+    set_cookies = requests.get(url)
     querystring = {"vin":vin_number,"lookupType":"vin"}
-    cookie = {"Ford-eTIS-locale":"language%3Den%2Ccountry%3DUS", "JSESSIONID":"jVYQWM13ZixazSeEQqfUkx_FxKsdPmu1difmIsg6.eccvas1900i"}
-
 
     headers = {
         'host': "www.etis.ford.com",
@@ -32,12 +30,44 @@ def get_car_details(vin_number):
         }
     print("trying to get the summary of ", vin_number)
     try:
-        car_details_html = requests.post(url, headers=headers, data=querystring, cookies=cookie)
+        car_details_html = requests.post(url, headers=headers, data=querystring, cookies=set_cookies.cookies)
     except:
         print("failed")
         return False
     car_details_soup = BeautifulSoup(car_details_html.content, "html.parser")
-    # print (car_details_soup)
-    something = car_details_soup.find("title")
-    print(something)
-get_car_details("1FA6P8JZXH5524472")
+    print(car_details_soup.find("title").get_text()) # shoule be Vehical Summary
+    primary_features_section = car_details_soup.find(id="pfcSummary")
+    primary_features = primary_features_section.find_all(class_="summaryContent")
+    options = {}
+    options["Build Date"] = primary_features[0].get_text().split("\xa0\xa0")[1]
+    options["Color"] = primary_features[-1].get_text()
+
+    #now to the minor feature list
+    #Vehicle Cover B == painted black roof
+    #Accent Stripe-<data I want> = Stripe
+    #SVT-R Package = R model
+    #With Temp Control Driver Seat = convenience Package
+    #With Navigation System && Less Temp Control Driver Seat = Electronics Package
+    minor_features_section = car_details_soup.find(id="mfcList")
+    minor_features = minor_features_section.find_all("li")
+    for feature in minor_features:
+        feature = feature.get_text()
+        if "Vehicle Cover B" in feature:
+            options["Painted Black Roof"] = True
+        if "Accent Stripe-" in feature:
+            options["Over the Top Racing Stripe"] = feature.split("-")[1]
+        if "With Temp Control Driver Seat" in feature:
+            options["Convenience Package"] = True
+        if "With Navigation" in feature:
+            options["has nav"] = True
+        if "SVT-R Package" in feature:
+            options["R Model"] = True
+
+    #check for electronic or convienence Package
+    if "has nav" in options and "Convenience Package" not in options:
+        options.pop("has nav", None)
+        options["Electronics Pacakge"] = True
+    elif "Convenience Pacakge" in options:
+        options.pop("has nav", None)
+
+    return options
